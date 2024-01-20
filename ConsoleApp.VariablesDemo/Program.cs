875c5fea1f1bfc0c0537ca190b90301468220670
Zelly Irigon <zellyirigon@hotmail.com>
2024-01-20 20:51:20 +1300

    Add project files.

---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Zelly Irigon";
+Console.WriteLine(" I am " + name); //String concatenation
+Console.WriteLine($"They call me {name}"); //string interpolation
+Console.WriteLine(" I was give the name {0}", name); //Formated string
+
+int age = 40;
+int retirementYearsLeft = 25;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is {0}", age);
+Console.WriteLine("My retirement age is {0}", retirementAge);
+
+bool isRetired = false;
+Console.WriteLine("Am I retired? {0}", isRetired);
